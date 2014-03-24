df8e4bd4bc9d1c86c0bbd5a5e0aa64e11f3869c2
Erik Cunningham <djtrinary@gmail.com>
2014-03-23 18:45:46 -0600

    augh failure

--- main.hs
+++ main.hs
@@ -1,5 +1,7 @@
 import Text.ParserCombinators.Parsec hiding (spaces)
 import System.Environment
+import Control.Monad
+import Numeric
 
 data LispVal = Atom String
              | List [LispVal]
@@ -9,20 +11,80 @@ data LispVal = Atom String
              | Bool Bool
 
 symbol :: Parser Char
-symbol = oneOf "!#$%&|*+-/:<=>?@^_~"
+symbol = oneOf "!$%&|*+-/:<=>?@^_~"
 
 spaces :: Parser ()
 spaces = skipMany1 space
 
+escapedChars :: Parser Char
+escapedChars = do
+  char '\\' -- a backslash
+  x <- oneOf "\\\"nrt" -- escapable characters
+  return $ case x of
+    '\\' -> x
+    '"' -> x
+    'n' -> '\n'
+    'r' -> '\r'
+    't' -> '\t'
+
+
 parseString :: Parser LispVal
 parseString = do
   char '"'
-  x <- many (noneOf "\"")
+  x <- many $ escapedChars <|> noneOf "\"\\"
   char '"'
   return $ String x
 
+parseBool :: Parser LispVal
+parseBool = do
+  char '#'
+  (char 't' >> return (Bool True)) <|> (char 'f' >> return (Bool False))
+
+parseAtom :: Parser LispVal
+parseAtom = do
+  first <- letter <|> symbol
+  rest <- many (letter <|> digit <|> symbol)
+  let atom = first:rest
+  return $ Atom atom
+
+parseDigital1 :: Parser LispVal
+parseDigital1 = many1 digit >>= (return . Number . read)
+
+parseDigital2 :: Parser LispVal
+parseDigital2 = do
+  string "#d"
+  many1 digit >>= (return . Number . read)
+
+parseOct :: Parser LispVal
+parseOct =
+  string "#o" >> many1 octDigit >>= (return . Number . readOct)
+
+parseBin :: Parser LispVal
+parseBin = do
+  string "#b"
+  return . Number . read
+
+parseHex :: Parser LispVal
+parseHex = do
+  try string "#x"
+  x <- many1 hexDigit
+  return $ Number (readHex x)
+
+parseNumber :: Parser LispVal
+parseNumber = parseDigital1
+           <|> parseDigital2
+           <|> parseHex
+           <|> parseOct
+           <|> parseBin
+
+parseExpr :: Parser LispVal
+parseExpr = parseAtom
+         <|> parseString
+         <|> parseBool
+         <|> parseNumber
+
 readExpr :: String -> String
-readExpr input = case parse (spaces >> symbol) "lisp" input of
+readExpr input = case parse parseExpr "lisp" input of
   Left err -> "No match: " ++ show err
   Right val -> "Found value"
 
